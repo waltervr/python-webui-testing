6785caca99934a9ff54c7f7671e8ae1353919579
CENTIC21 <yiyax97405@onzmail.com>
2021-04-05 09:19:31 -0700

    completed step 4

--- tests/test_web.py
+++ tests/test_web.py
@@ -3,22 +3,17 @@ import pytest
 from pages.result import DuckDuckGoResultPage
 from pages.search import DuckDuckGoSearchPage
 
-from selenium.webdriver import Chrome
-
-@pytest.fixture
-def browser():
-    driver = Chrome()
-    driver.implicitly_wait(10)
-    yield driver
-    driver.quit()
 
 def test_basic_duckduckgo_search(browser):
+    # Set up test case data
     PHRASE = "panda"
 
+    # Search for the phrase
     search_page = DuckDuckGoSearchPage(browser)
     search_page.load()
     search_page.search(PHRASE)
 
+    # Verify that results appear
     result_page = DuckDuckGoResultPage(browser)
     assert result_page.link_div_count() > 0
     assert result_page.phrase_result_count(PHRASE) > 0
