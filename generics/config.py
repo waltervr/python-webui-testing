e1a1b8931d7fd88233252f322c8c3d6406c80d88
waltervr <walter1008@gmail.com>
2021-04-20 09:04:53 -0700

    completed step 7 basepage

---
+++ generics/config.py
@@ -0,0 +1,4 @@
+class TestData:
+    BROWSER = "chrome"
+    WAIT_TIME = 10
+    APP_URL = ""
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,48 +1,29 @@
 import pytest
-import json
-
 from selenium.webdriver import Chrome, Firefox
+from generics.config import TestData
 
-CONFIG_PATH = 'tests/config.json'
-DEFAULT_WAIT_TIME = 10
-SUPPORTED_BROWSERS = ['chrome', 'firefox']
 
-@pytest.fixture(scope='session')
-def config():
-    # Read the JSON config file and returns it as a parsed dict
-    with open(CONFIG_PATH) as config_file:
-        data = json.load(config_file)
-    return data
-
-@pytest.fixture(scope='session')
-def config_browser(config):
-    # Validate and return the browser choice from the config data
-    if 'browser' not in config:
-        raise Exception('The config file does not contain "browser"')
-    elif config['browser'] not in SUPPORTED_BROWSERS:
-        raise Exception(f'"{config["browser"]}" is not a supported browser')
-    return config['browser']
-
-@pytest.fixture(scope='session')
-def config_wait_time(config):
-    # Validate and return the wait time from the config data
-    return config['wait_time'] if 'wait_time' in config else DEFAULT_WAIT_TIME
-
-@pytest.fixture
-def browser(config_browser, config_wait_time):
+@pytest.fixture(scope='class')
+def init_driver(request):
     # Initialize WebDriver
-    if config_browser == 'chrome':
-        driver = Chrome()
-    elif config_browser == 'firefox':
-        driver = Firefox()
+    if TestData.BROWSER == "chrome":
+        web_driver = Chrome()
+    elif TestData.BROWSER == "firefox":
+        web_driver = Firefox()
     else:
-        raise Exception(f'"{config_browser}" is not supported browser')
+        raise Exception(f'"{TestData.BROWSER}" is not supported browser')
     
-    # Wait implicitly for elements to be ready before attempting interactions
-    driver.implicitly_wait(config_wait_time)
+    # Create a global instance of the driver for every execution
+    request.cls.driver = web_driver
+
+    # Maximize the window
+    web_driver.maximize_window()
+
+    # Open the URL
+    web_driver.get(TestData.APP_URL)
 
     # Return the driver object at the end of setup
-    yield driver
+    yield web_driver
     
     # For cleanup, quit the driver
-    driver.quit()
+    web_driver.quit()
